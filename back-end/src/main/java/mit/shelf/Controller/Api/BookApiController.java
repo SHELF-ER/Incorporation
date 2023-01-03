62fc0468d668eab50e2eb3a490d4f87cbafe4d4c
woogi <dbwhddnr00@naver.com>
2023-01-03 22:13:32 +0900

    excel 생성중

--- back-end/src/main/java/mit/shelf/Controller/Api/BookApiController.java
+++ back-end/src/main/java/mit/shelf/Controller/Api/BookApiController.java
@@ -2,12 +2,23 @@ package mit.shelf.Controller.Api;
 
 import mit.shelf.Form.MemberForm;
 import mit.shelf.domain.Book;
+import mit.shelf.domain.ExcelData;
 import mit.shelf.repository.BookRepository;
 import mit.shelf.service.BookService;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.json.simple.JSONObject;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -82,5 +93,39 @@ public class BookApiController {
         bookRepository.deleteById(id);
         result.put("result", "success");
         return result;
+    }
+
+    @PostMapping("/books/excel")
+    public String readExcel(@RequestParam("file") MultipartFile file, Model model)
+            throws IOException {
+
+        List<ExcelData> dataList = new ArrayList<>();
+        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+
+        if (!extension.equals("xlsx") && !extension.equals("xls")) {
+            throw new IOException("엑셀파일만 업로드 해주세요.");
+        }
+
+        Workbook workbook = null;
+
+        if (extension.equals("xlsx")) {
+            workbook = new XSSFWorkbook(file.getInputStream());
+        } else if (extension.equals("xls")) {
+            workbook = new HSSFWorkbook(file.getInputStream());
+        }
+
+        Sheet worksheet = workbook.getSheetAt(0);
+
+        for (int i = 1; i < worksheet.getPhysicalNumberOfRows(); i++) {
+            Row row = worksheet.getRow(i);
+            Book book = new Book();
+            book.setBookNum((int) row.getCell(0).getNumericCellValue());
+            book.setBorrower(row.getCell(1).getStringCellValue());
+            book.setName(row.getCell(2).getStringCellValue());
+            bookService.join(book);
+        }
+        model.addAttribute("datas", dataList);
+        return "home";
+
     }
 }
